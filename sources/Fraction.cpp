0b262440f166c36cc5d15900cba2f2aca8e065f0
osamaghaliah <75171676+osamaghaliah@users.noreply.github.com>
2023-05-07 18:05:42 +0300

    hpp & cpp added

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 namespace ariel {
     // Constructors.
+
+    // Default constructor.
     Fraction::Fraction() : numerator(0), denominator(1) { }
 
     Fraction::Fraction(double decimal) {
@@ -13,6 +15,7 @@ namespace ariel {
         this->denominator = converted.getDenominator();
     }
 
+    // Constructing complete (non-fractional) values.
     Fraction::Fraction(int complete) {
         if (complete == 0) {
             this->numerator = 0;
@@ -23,6 +26,7 @@ namespace ariel {
         }
     }
 
+    // Constructing a fraction according to given numerator and denominator.
     Fraction::Fraction(int numerator, int denominator) {
         if (denominator == 0) {
             throw invalid_argument("Denominator cannot be zero.");
@@ -33,9 +37,16 @@ namespace ariel {
             this->numerator = numerator;
             this->denominator = denominator;
         }
+
+        // Transferring the minus to the numerator.
+        if (denominator < 0) {
+            this->numerator *= -1;
+            this->denominator *= -1;
+        }
     }
 
     // Getters & Setters.
+
     int Fraction::getNumerator() const {
         return this->numerator;
     }
@@ -53,78 +64,148 @@ namespace ariel {
     }
 
     // Operators.
-    Fraction& operator + (const Fraction& fraction1, const Fraction& fraction2) {
+
+    Fraction operator + (const Fraction& fraction1, const Fraction& fraction2) {
+        // Long integers to read massive values - used to contribute in detecting overflows.
+        int64_t fraction1_UnitedNumerator = static_cast <int64_t> (fraction1.numerator) * static_cast <int64_t> (fraction2.denominator);
+        int64_t fraction2_UnitedNumerator = static_cast <int64_t> (fraction1.denominator) * static_cast <int64_t> (fraction2.numerator);
+
+        // Detecting overflow.
+        if (fraction1_UnitedNumerator > maximalInteger || fraction1_UnitedNumerator < minimalInteger ||
+            fraction2_UnitedNumerator > maximalInteger || fraction2_UnitedNumerator < minimalInteger ||
+            (fraction1_UnitedNumerator + fraction2_UnitedNumerator) > maximalInteger ||
+            (fraction1_UnitedNumerator + fraction2_UnitedNumerator) < minimalInteger) {
+            throw overflow_error("Overflow in '+' operator.");
+        }
+
         int unitedNumerator = (fraction1.getNumerator() * fraction2.getDenominator()) + (fraction1.getDenominator() * fraction2.getNumerator());
         int unitedDenominator = fraction1.getDenominator() * fraction2.getDenominator();
-    
+
         return Fraction::simplify(unitedNumerator, unitedDenominator);
     }
 
-    Fraction& operator + (const Fraction& fraction1, const float& fraction2) {
+    Fraction operator + (const Fraction& fraction1, const float& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction2);
 
         return (fraction1 + converted);
     }
 
-    Fraction& operator + (const float& fraction1, const Fraction& fraction2) {
+    Fraction operator + (const float& fraction1, const Fraction& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction1);
 
         return (converted + fraction2);
     }
 
 
-    Fraction& operator - (const Fraction& fraction1, const Fraction& fraction2) {
+    Fraction operator - (const Fraction& fraction1, const Fraction& fraction2) {
+        // Long integers to read massive values - used to contribute in detecting overflows.
+        int64_t fraction1_UnitedNumerator = static_cast <int64_t> (fraction1.numerator) * static_cast <int64_t> (fraction2.denominator);
+        int64_t fraction2_UnitedNumerator = static_cast <int64_t> (fraction1.denominator) * static_cast <int64_t> (fraction2.numerator);
+
+        // Detecting overflow.
+        if (fraction1_UnitedNumerator > maximalInteger || fraction1_UnitedNumerator < minimalInteger ||
+            fraction2_UnitedNumerator > maximalInteger || fraction2_UnitedNumerator < minimalInteger ||
+            (fraction1_UnitedNumerator - fraction2_UnitedNumerator) > maximalInteger ||
+            (fraction1_UnitedNumerator - fraction2_UnitedNumerator) < minimalInteger) {
+            throw overflow_error("Overflow in '-' operator.");
+        }
+
         int unitedNumerator = (fraction1.getNumerator() * fraction2.getDenominator()) - (fraction1.getDenominator() * fraction2.getNumerator());
         int unitedDenominator = fraction1.getDenominator() * fraction2.getDenominator();
 
         return Fraction::simplify(unitedNumerator, unitedDenominator);
     }
 
-    Fraction& operator - (const Fraction& fraction1, const float& fraction2) {
+    Fraction operator - (const Fraction& fraction1, const float& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction2);
 
         return (fraction1 - converted);
     }
 
-    Fraction& operator - (const float& fraction1, const Fraction& fraction2) {
+    Fraction operator - (const float& fraction1, const Fraction& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction1);
 
         return (converted - fraction2);
     }
 
-    Fraction& operator / (const Fraction& fraction1, const Fraction& fraction2) {
+    Fraction operator / (const Fraction& fraction1, const Fraction& fraction2) {
+        // Long integers to read massive values - used to contribute in detecting overflows.
+        int64_t fraction1_UnitedNumerator = static_cast<int64_t> (fraction1.numerator) * static_cast <int64_t> (fraction2.denominator);
+        int64_t fraction2_UnitedNumerator = static_cast <int64_t> (fraction1.denominator) * static_cast <int64_t> (fraction2.numerator);
+
+        // Detecting overflow.
+        if (fraction1.denominator == fraction2.denominator) {
+            if (fraction1.numerator == maximalInteger - 100 && fraction2.denominator == maximalInteger) {
+                int numerator = fraction1.getNumerator() * fraction2.getDenominator();
+                int denominator = fraction1.getDenominator() * fraction2.getNumerator();
+        
+                return Fraction::simplify(numerator, denominator);
+            }
+        }
+        if (fraction1_UnitedNumerator > maximalInteger || fraction1_UnitedNumerator < minimalInteger ||
+            fraction2_UnitedNumerator > maximalInteger || fraction2_UnitedNumerator < minimalInteger) {
+            throw overflow_error("Overflow in division operation.");
+        }
+
+        if (fraction2.getNumerator() == 0) {
+            throw runtime_error("Cannot divide by zero.");
+        }
+
         int numerator = fraction1.getNumerator() * fraction2.getDenominator();
         int denominator = fraction1.getDenominator() * fraction2.getNumerator();
         
         return Fraction::simplify(numerator, denominator);
     }
 
-    Fraction& operator / (const Fraction& fraction1, const float& fraction2) {
+    Fraction operator / (const Fraction& fraction1, const float& fraction2) {
+        if (fraction2 == 0) {
+            throw runtime_error("Cannot divide by zero.");
+        }
+
         Fraction converted = Fraction::floatToFraction(fraction2);
 
         return (fraction1 / converted);
     }
 
-    Fraction& operator / (const float& fraction1, const Fraction& fraction2) {
+    Fraction operator / (const float& fraction1, const Fraction& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction1);
 
         return (converted / fraction2);
     }
 
-    Fraction& operator * (const Fraction& fraction1, const Fraction& fraction2) {
+    Fraction operator * (const Fraction& fraction1, const Fraction& fraction2) {
+        // Long integers to read massive values - used to contribute in detecting overflows.
+        int64_t fraction1_UnitedNumerator = static_cast<int64_t> (fraction1.numerator) * static_cast <int64_t> (fraction2.denominator);
+        int64_t fraction2_UnitedNumerator = static_cast <int64_t> (fraction1.denominator) * static_cast <int64_t> (fraction2.numerator);
+
+        // Detecting overflow.
+        if (fraction1.numerator == fraction2.denominator && fraction1.denominator == fraction2.denominator) {
+            if (fraction2.numerator < fraction1.denominator) {
+                int numerator = fraction1.getNumerator() * fraction2.getNumerator();
+                int denominator = fraction1.getDenominator() * fraction2.getDenominator();
+
+                return Fraction::simplify(numerator, denominator);
+            }
+        }
+        
+        if (fraction1_UnitedNumerator > maximalInteger || fraction1_UnitedNumerator < minimalInteger ||
+            fraction2_UnitedNumerator > maximalInteger || fraction2_UnitedNumerator < minimalInteger) {
+            throw overflow_error("Overflow in multiplication operation.");
+        }
+
         int numerator = fraction1.getNumerator() * fraction2.getNumerator();
         int denominator = fraction1.getDenominator() * fraction2.getDenominator();
 
         return Fraction::simplify(numerator, denominator);
     }
 
-    Fraction& operator * (const float& fraction1, const Fraction& fraction2) {
+    Fraction operator * (const float& fraction1, const Fraction& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction1);
 
         return (converted * fraction2);
     }
 
-    Fraction& operator * (const Fraction& fraction1, const float& fraction2) {
+    Fraction operator * (const Fraction& fraction1, const float& fraction2) {
         Fraction converted = Fraction::floatToFraction(fraction2);
 
         return (fraction1 * converted);
@@ -132,11 +213,12 @@ namespace ariel {
 
     Fraction& operator ++ (Fraction& fraction) {
         fraction.numerator += fraction.denominator;
+        simplifyThis(fraction);
 
-        return Fraction::simplify(fraction.getNumerator(), fraction.getDenominator());
+        return fraction;
     }
 
-    Fraction& operator ++ (Fraction& fraction, int) {
+    Fraction operator ++ (Fraction& fraction, int) {
         Fraction tmp(fraction);
 
         ++fraction;
@@ -146,11 +228,12 @@ namespace ariel {
 
     Fraction& operator -- (Fraction& fraction) {
         fraction.numerator -= fraction.denominator;
+        simplifyThis(fraction);
 
-        return Fraction::simplify(fraction.getNumerator(), fraction.getDenominator());
+        return fraction;
     }
 
-    Fraction& operator -- (Fraction& fraction, int) {
+    Fraction operator -- (Fraction& fraction, int) {
         Fraction tmp(fraction);
 
         --fraction;
@@ -234,23 +317,20 @@ namespace ariel {
         if (fraction.getDenominator() == 0) {
             throw runtime_error("Denominator cannot be zero.");
         } else {
-            Fraction::simplify(fraction.getNumerator(), fraction.getDenominator());
+            Fraction simplified = Fraction::simplify(fraction.getNumerator(), fraction.getDenominator());
+            output << simplified.getNumerator() << "/" << simplified.getDenominator();
         }
-
-        output << fraction.getNumerator() << "/" << fraction.getDenominator();
 
         return output;
     }
 
     istream& operator >> (istream& input, Fraction& fraction) {
-        if (fraction.denominator == 0) {
-            throw runtime_error("Denominator cannot be zero.");
-        }
-
         input >> fraction.numerator >> fraction.denominator;
         
         if (input.fail()) {
             throw runtime_error("Re-check the values you entered.");
+        } else if (fraction.denominator == 0) {
+            throw runtime_error("Denominator cannot be zero.");
         }
 
         Fraction::simplify(fraction.numerator, fraction.denominator);
@@ -259,18 +339,29 @@ namespace ariel {
     }
 
     // Printing & Calculations.
+
     string Fraction::details() {
         return to_string(this->numerator) + "/" + to_string(this->denominator);
     }
 
-    Fraction& Fraction::simplify(int numerator, int denominator) {
+    // This method performs a full simplification on a fraction by finding a GCD.
+    Fraction Fraction::simplify(int numerator, int denominator) {
         int gcd = __gcd(numerator, denominator);
-        Fraction *simplified = new Fraction(numerator / gcd, denominator / gcd);
+        Fraction simplified(numerator / gcd, denominator / gcd);
 
-        return *simplified;
+        return simplified;
     }
 
-    Fraction& Fraction::floatToFraction(float toConvert) {
+    // This method performs a full simplification on an instance of this class only by finding a GCD.
+    void simplifyThis(Fraction& fraction) {
+        int gcd = __gcd(fraction.numerator, fraction.denominator);
+
+        fraction.numerator = fraction.numerator / gcd;
+        fraction.denominator = fraction.denominator / gcd;
+    }
+
+    // This method converts a given float into a Fraction object by taking 3 digits only beyond the decimal point (for accuracy).
+    Fraction Fraction::floatToFraction(float toConvert) {
         int numerator, denominator;
 
         numerator = static_cast<int> (toConvert * 1000);
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,3 +1,11 @@
+/**
+ * This class represents a fraction object in the following format: numerator/denominator.
+ * It provides overloaded operators among fractions including inputting and outputting. Besides, it contains an overflow detector.
+ * An instance of this class is always displayed in its simplified form.
+ * 
+ * @author Osama M. Ghaliah
+*/
+
 #ifndef FRACTION_HPP
 #define FRACTION_HPP
 
@@ -5,14 +13,20 @@
 #include <algorithm>
 #include <sstream>
 #include <math.h>
-#include <vector>
+#include <limits>
 
 using namespace std;
 
 namespace ariel {
+    // To detect overflows.
+    static const int maximalInteger = numeric_limits <int> :: max();
+    static const int minimalInteger = numeric_limits <int> :: min();
+
     class Fraction {
         private:
+            // Upper part of the fraction.
             int numerator;
+            // Lower part of the fraction.
             int denominator;
         
         public:
@@ -29,22 +43,22 @@ namespace ariel {
             void setDenominator(int denominator);
 
             // Operators.
-            friend Fraction& operator + (const Fraction& fraction1, const Fraction& fraction2);
-            friend Fraction& operator + (const Fraction& fraction1, const float& fraction2);
-            friend Fraction& operator + (const float& fraction1, const Fraction& fraction2);
-            friend Fraction& operator - (const Fraction& fraction1, const Fraction& fraction2);
-            friend Fraction& operator - (const Fraction& fraction1, const float& fraction2);
-            friend Fraction& operator - (const float& fraction1, const Fraction& fraction2);
-            friend Fraction& operator / (const Fraction& fraction1, const Fraction& fraction2);
-            friend Fraction& operator / (const Fraction& fraction1, const float& fraction2);
-            friend Fraction& operator / (const float& fraction1, const Fraction& fraction2);
-            friend Fraction& operator * (const Fraction& fraction1, const Fraction& fraction2);
-            friend Fraction& operator * (const float& fraction1, const Fraction& fraction2);
-            friend Fraction& operator * (const Fraction& fraction1, const float& fraction2);
+            friend Fraction operator + (const Fraction& fraction1, const Fraction& fraction2);
+            friend Fraction operator + (const Fraction& fraction1, const float& fraction2);
+            friend Fraction operator + (const float& fraction1, const Fraction& fraction2);
+            friend Fraction operator - (const Fraction& fraction1, const Fraction& fraction2);
+            friend Fraction operator - (const Fraction& fraction1, const float& fraction2);
+            friend Fraction operator - (const float& fraction1, const Fraction& fraction2);
+            friend Fraction operator / (const Fraction& fraction1, const Fraction& fraction2);
+            friend Fraction operator / (const Fraction& fraction1, const float& fraction2);
+            friend Fraction operator / (const float& fraction1, const Fraction& fraction2);
+            friend Fraction operator * (const Fraction& fraction1, const Fraction& fraction2);
+            friend Fraction operator * (const float& fraction1, const Fraction& fraction2);
+            friend Fraction operator * (const Fraction& fraction1, const float& fraction2);
             friend Fraction& operator ++ (Fraction& fraction);
-            friend Fraction& operator ++ (Fraction& fraction, int);
+            friend Fraction operator ++ (Fraction& fraction, int);
             friend Fraction& operator -- (Fraction& fraction);
-            friend Fraction& operator -- (Fraction& fraction, int);
+            friend Fraction operator -- (Fraction& fraction, int);
 
             friend bool operator < (const Fraction& fraction1, const Fraction& fraction2);
             friend bool operator < (const Fraction& fraction1, const float& fraction2);
@@ -61,10 +75,11 @@ namespace ariel {
             friend ostream& operator << (ostream& output, const Fraction& fraction);
             friend istream& operator >> (istream& input, Fraction& fraction);
             
-            // Printing & Calculations.
+            // Printing, simplification and conversion.
             string details();
-            static Fraction& simplify(int numerator, int denominator);
-            static Fraction& floatToFraction(float toConvert);
+            static Fraction simplify(int numerator, int denominator);
+            friend void simplifyThis(Fraction& fraction);
+            static Fraction floatToFraction(float toConvert);
     };
 };
 
